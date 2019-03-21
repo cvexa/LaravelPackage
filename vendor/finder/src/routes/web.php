9d10cd7d6ee0485df0b3de21086fa34c5eeb3c00
cvexa <cvexxxxa@abv.bg>
2019-03-21 15:46:27 +0200

    created package

---
+++ vendor/finder/src/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+Route::group(['namespace' => 'vendor\finder\Http\controllers', 'middleware' => ['web']], function () {
+    Route::get('find', 'FinderController@index')->name('find');
+    Route::post('search', 'FinderController@search')->name('find.me');
+});

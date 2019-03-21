366958d2a4cd1ead52955ead0e927902ae032cc4
cvexa <cvexxxxa@abv.bg>
2019-03-21 21:58:24 +0200

    renamed the whole folder from vendor to cvexa

---
+++ cvexa/finder/src/FinderServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+    // MyVendor\contactform\src\ContactFormServiceProvider.php
+namespace cvexa\finder;
+
+use Illuminate\Support\ServiceProvider;
+
+class FinderServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'finder');
+        app()->config["filesystems.disks.publicDisk"] = [
+            'driver' => 'local',
+            'root' => public_path(),
+        ];
+    }
+    public function register()
+    {
+    }
+}
---
+++ cvexa/finder/src/resources/views/finder.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+          <title>Search by keywords</title>
+          <style media="screen">
+              li{
+                  overflow-wrap:break-word;
+              }
+          </style>
+    </head>
+        <body>
+
+          <div style="width: 500px; margin: 0 auto; margin-top: 90px;">
+            @if(session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+          <h3>Search keyword in files</h3>
+
+          <form action="{{route('find.me')}}" method="POST">
+              @csrf
+              <div class="form-group">
+                <label for="exampleFormControlInput1">What to search</label>
+                <input type="text" class="form-control" name="search"  placeholder="keyword">
+              </div>
+
+              <div class="form-group">
+                <label for="exampleFormControlInput1">Where to search</label>
+                <select class="" name="location">
+                    <option value=0>all</option>
+                    @foreach($publicDirectories as $dir)
+                        <option value="{{$dir}}">{{$dir}}</option>
+                    @endforeach
+                </select>
+              </div>
+
+              @if(isset($output) && !empty($output))
+                  <div class="form-group alert alert-success">
+                      Results:
+                      <ol>
+                      @foreach($output as $result)
+                          <li>
+                              {{$result}}
+                          </li>
+                      @endforeach
+                    </ol>
+                  </div>
+              @else
+                  <div class="form-group alert alert-success">
+                      No Results
+                  </div>
+              @endif
+
+              <button type="submit" class="btn btn-primary" style="width: 500px; margin: 0 auto; margin-top: 10px;">Search</button>
+         </form>
+        </div>
+    </body>
+    </html>

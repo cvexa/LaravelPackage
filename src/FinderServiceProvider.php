4087646433d164c903d156e465c804360c023536
cvexa <cvexxxxa@abv.bg>
2019-03-22 15:56:04 +0200

    install dusk artisan command

--- src/FinderServiceProvider.php
+++ src/FinderServiceProvider.php
@@ -14,6 +14,7 @@ class FinderServiceProvider extends ServiceProvider
             'driver' => 'local',
             'root' => public_path(),
         ];
+        Artisan::call('dusk:install');
     }
     public function register()
     {

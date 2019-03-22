885bcf22080c0fc251869470f92d95ab164a6c57
cvexa <cvexxxxa@abv.bg>
2019-03-22 16:17:03 +0200

    basic Dusk test

---
+++ src/Tests/Browser/searchTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
+use Carbon\Carbon;
+use Tests\Browser\Components\InputCases;
+
+class searchTest extends DuskTestCase
+{
+    use WithFaker;
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testRegister()
+    {
+        $this->faker = \Faker\Factory::create();
+
+        $this->browse(function (Browser $browser) use ($inputCases) {
+            $browser->visit('/find');
+            $browser->waitForText('Search keyword in files');
+        });
+    }
+}
